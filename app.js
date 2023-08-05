97b523569dc1b98979ec9ddee492d118f0e240a7
And1298 <andrealanciano@me.com>
2023-08-05 23:42:17 +0200

    irst try with map - she wons

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-import { TiledMap } from 'pixi-tiledmap';
+// import { TiledMap } from 'pixi-tiledmap';
+// import { TiledMap } from './node_modules/';
+// import { TilingSprite } from '@pixi/sprite-tiling';
+// import { TilingSprite } from './node_modules/@pixi/sprite-tiling/lib/TilingSprite.mjs';
+// const PixiTiledMap = require('pixi-tiledmap').TiledMap;
+
 // Inizializza PixiJS
 const app = new PIXI.Application({
   resizeTo: window,
   backgroundColor: 0xaaaaaa,
 });
+// PIXI.loader
+//     .add('assets/01_basement.tmx')
+//     .load(() => {
+//         /**
+//          *   PIXI.extras.TiledMap() is an extended PIXI.Container()
+//          *   so you can render it right away
+//          */
+//         renderer.render(new PIXI.extras.TiledMap('assets/01_basement.tmx'));
+//     });
 //inizializzazione TMX
 // const PIXI = require('pixi.js');
 // const PixiTiledMap = require('pixi-tiledmap');
@@ -18,7 +32,7 @@ const bunny = PIXI.Sprite.from("https://pixijs.com/assets/bunny.png");
 // center the sprite's anchor point
 // bunny.anchor.set(0.5);
 
-let playerLevel = 1;
+let playerLevel = 0;
 let playerXP = 0;
 const XPToNextLevel = [100, 250, 500, 1000, 2000 /* Altri valori... */];
 
@@ -44,7 +58,7 @@ function checkLevelUp() {
 
 // Aggiungi un listener per l'evento 'keydown' della tastiera
 document.addEventListener("keydown", (event) => {
-  let speed = 10; // La velocità di movimento del personaggio
+  let speed = 20; // La velocità di movimento del personaggio
 
   // Controlla il tasto premuto
   if (
