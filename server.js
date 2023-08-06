fe57cf9ba698c13844a74a1e01dc8bcd8fb22374
And1298 <andrealanciano@me.com>
2023-08-06 11:42:05 +0200

    prova con Tilemap

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = 'C:/Users/andre/wa_hack70/pixi';
 const app = express();
 const port = 3000; // Puoi modificare la porta a tua discrezione
 
 // Configura Express per servire i file statici nella cartella del tuo progetto PixiJS
-app.use(express.static('C:/Users/andre/wa_hack70/pixi'));
+app.use(express.static(path));
 
 // Avvia il server
 app.listen(port, () => {
